ed2152a8a58ec3c7001314122d9e2707425f400a
edelvarden <edelvarden@gmail.com>
2023-08-04 02:52:30 +0300

    move to batch file

--- install.bat
+++ install.bat
@@ -9,8 +9,23 @@ fltmc >nul 2>&1 || (
     exit 0
 )
 
-PowerShell -ExecutionPolicy Unrestricted -File "install.ps1"
+@REM Base installation
+wsl.exe -d Ubuntu --user root sh -c "bash install.sh"
+wsl.exe -d Ubuntu --user root sh -c "npm install --global yarn typescript"
 
+
+@REM Install ohmyposh with json theme | https://ohmyposh.dev/
+wsl.exe -d Ubuntu --user root sh -c "curl -s https://ohmyposh.dev/install.sh | bash -s"
+wsl.exe -d Ubuntu sh -c "cat powerlevel10k_rainbow.omp.json > ~/powerlevel10k_rainbow.omp.json"
+wsl.exe -d Ubuntu sh -c "bash install_ohmyposh.sh"
+
+
+@REM Install docker
+wsl.exe -d Ubuntu --user root sh -c "bash install_docker.sh"
+
+
+echo ----------------------------------------------------------------
+echo Installation complete.
 
 pause
 @REM exit /b 0
